b18b3d104350698c6756860c93c28bbcf538d3b1
Sage Franch <sage@getcrescendo.co>
2019-04-25 12:21:15 -0400

    Adding Crescendo

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Crescendo',
+    img: 'crescendo.png',
+    link: 'https://getcrescendo.co'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
